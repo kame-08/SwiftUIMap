2df97f100a71aea40cc248656e08ca7d0c067f6b
Ryo <21cm0108@jec.ac.jp>
2022-08-30 12:23:48 +0900

    Ver.1

--- SwiftUIMap/ContentView.swift
+++ SwiftUIMap/ContentView.swift
@@ -6,11 +6,40 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+
+    @State private var region = MKCoordinateRegion()
+    
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Spacer()
+            
+            Text("経度\(region.center.longitude)")
+            Text("緯度\(region.center.latitude)")
+            
+            Spacer()
+            
+            Map(coordinateRegion: $region, interactionModes: [], showsUserLocation: true, userTrackingMode: $userTrackingMode)
+
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width / 2)
+                .mask(
+                    Image("HalfCircle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                )
+               
+        }
+        .ignoresSafeArea()
+        .onAppear(){
+            //位置報許可
+            let manager = CLLocationManager()
+            manager.requestWhenInUseAuthorization()
+        }
     }
 }
 
